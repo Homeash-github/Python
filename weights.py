364ae16805b09b988f6c23d943b570a2bf0691a6
Homeash Pranav V <93364869+Homeash-github@users.noreply.github.com>
2024-07-29 14:48:35 +0530

    Add files via upload

---
+++ weights.py
@@ -0,0 +1,7 @@
+N = int(input())
+for i in range(N):
+    w,x,y,z = map(int, input().split(" "))
+    if((x==w) or (y==w) or (z==w) or (x+y==w) or (x+z == w) or (y+z==w) or (x+y+z==w)):
+        print("YES")
+    else:
+        print("NO")
